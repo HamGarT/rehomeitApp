041f064e02d74b05d0b6ccddb7253a9ba4bc6b18
Hamer Garcia <hamgar28@gmail.com>
2026-09-14 18:30:28 -0500

    Add lib folder structure with core, data, features, and shared directories

---
+++ lib/data/models/item_model.dart
@@ -0,0 +1,11 @@
+class ItemModel {
+  final String id;
+  final String title;
+  final String description;
+
+  const ItemModel({
+    required this.id,
+    required this.title,
+    required this.description,
+  });
+}
